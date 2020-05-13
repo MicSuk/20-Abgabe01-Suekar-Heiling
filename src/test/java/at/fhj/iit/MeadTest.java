0d46935d093b798a16b2851124b8fa16334be44f
MicSuk <michael.suekar@edu.campus02.at>
2020-05-13 10:34:50 +0200

    Added a MeadTest.java to testfiles

---
+++ src/test/java/at/fhj/iit/MeadTest.java
@@ -0,0 +1,42 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Testing Mead implementation")
+class MeadTest {
+
+    @Test
+    void getIngredients() {
+    }
+
+    @Test
+    void setIngredients() {
+    }
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void getAlcoholPercent() {
+    }
+
+    @Test
+    void isAlcoholic() {
+    }
+
+    @Test
+    void drink() {
+    }
+
+    @Test
+    void addIngredient() {
+    }
+
+    @Test
+    void removeIngredient() {
+    }
+}
