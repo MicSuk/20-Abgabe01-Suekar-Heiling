88ce7254cf78e313d513e689473724b9d1ab0737
Marco Heiling <marco.heiling@edu.campus02.at>
2020-05-13 11:55:34 +0200

    Added Implementation of the SimpleDrinkTest

--- src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -1,5 +1,6 @@
 package at.fhj.iit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Testing SimpleDrink implementation")
 class SimpleDrinkTest {
 
-    @Test
-    void getVolume() {
+    private SimpleDrink sA;
+    private SimpleDrink sN;
+
+    /**
+     * inits an alcoholic and nonalcoholic SimpleDrink for EACH test
+     */
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        sN = new SimpleDrink("Cola", new Liquid("Pepsi", 0.5, 0));
+        sA = new SimpleDrink("Jägermeister", new Liquid("Jägermeister", 0.02, 35));
     }
 
     @Test
-    void getAlcoholPercent() {
+    @DisplayName("Testing Volume Getter")
+    void testGetterVolume() {
+        assertEquals(sN.getVolume(), 0.5, 0.001);
+        assertEquals(sA.getVolume(), 0.02, 0.001);
     }
 
     @Test
-    void isAlcoholic() {
+    @DisplayName("Testing Alcohol Percent Getter")
+    void testGetterAlcoholPercent() {
+        assertEquals(sN.getAlcoholPercent(), 0, 0.001);
+        assertEquals(sA.getAlcoholPercent(), 35, 0.001);
+    }
+
+    @Test
+    @DisplayName("Testing is Alcoholic")
+    void testIsAlcoholic() {
+        assertEquals(sN.isAlcoholic(), false);
+        assertEquals(sA.isAlcoholic(), true);
     }
 }
