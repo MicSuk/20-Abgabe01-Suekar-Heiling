bb65a19dedbbad7bb34c557dc5f1c78958e7ffc8
Marco Heiling <marco.heiling@edu.campus02.at>
2020-05-13 10:37:55 +0200

    Auto generated SimpleDrinkTest

---
+++ src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -0,0 +1,22 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Testing SimpleDrink implementation")
+class SimpleDrinkTest {
+
+    @Test
+    void getVolume() {
+    }
+
+    @Test
+    void getAlcoholPercent() {
+    }
+
+    @Test
+    void isAlcoholic() {
+    }
+}
